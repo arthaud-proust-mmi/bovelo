858983420c7a73b41c483ca6586138a4382dfbe5
Arthaud Proust <arthaud.proust@mink-agency.com>
2022-10-26 09:42:44 +0200

    add colors

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -6,6 +6,32 @@ module.exports = {
     "./content/**/*.html"
   ],
   theme: {
+    colors: {
+      'primary': {
+        '1': '#5AB3FA',
+        '2': '#82f3d6',
+        '3': '#434ea8',
+      },
+      'secondary': {
+        '1': '#FFF7AD',
+        '2': '#FFF280',
+      },
+      'tertiary': {
+        '1': '#A4FF8E',
+      },
+      'cta': {
+        '1': '#FCB64F',
+        '2': '#FBA11D',
+      },
+      'background': {
+        '1': '#FDF3E6',
+        '2': '#FBE8D0',
+      },
+      'text': {
+        '1': '#1A1A1A',
+        '2': '#313131',
+      },
+    },
     extend: {
 
     },
