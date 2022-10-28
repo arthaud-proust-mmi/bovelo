5d3cc4a56ed7ea8315dca9139d802d86778595a4
louis lasserre <louis.lasserre@mmibordeaux.com>
2022-10-28 10:31:12 +0200

    marquis is moving

---
+++ static/js/main.js
@@ -0,0 +1,42 @@
+
+let loaded = false
+let pageHeight = null
+let actualPercent = 0
+let actualPx = 0
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  //console.log('body', document.body.scrollHeight);
+
+  pageHeight = document.body.scrollHeight - window.innerHeight
+
+  loaded = true
+  actualPerCent= (actualPx * 100) / pageHeight
+  moveMarquis(0.0, 20.0)
+})
+
+document.addEventListener("scroll", () => {
+
+  if (loaded) {
+
+    actualPx = window.pageYOffset
+    actualPerCent= (actualPx * 100) / pageHeight
+
+    moveMarquis(0.0, 22.0)
+  }
+})
+
+
+
+function moveMarquis(min, max) {
+  const marquis = document.querySelector('.marquis')
+  if (actualPerCent <= max && actualPerCent >= min) {
+
+
+    //marquis.style.left = 'calc(40% + ' + (actualPerCent * 10) + 'px)'
+
+    marquis.style.transform = 'rotate(-12deg) translate(calc(-60% + ' + (actualPerCent * 10) + 'px), 0)'
+
+  }
+
+}
